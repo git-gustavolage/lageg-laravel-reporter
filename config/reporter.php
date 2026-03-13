8170e3fb7b43ca4678c8f094022ed069b854ce7a
07233627266 <lageg901@gmail.com>
2026-03-13 09:10:28 -0400

    update: default config

--- config/reporter.php
+++ config/reporter.php
@@ -2,12 +2,36 @@
 
 return [
 
+    /**
+     * The default driver
+     */
     'default_driver' => 'pdf',
 
+
+    /**
+     * Configuration of the default suported drivers: pdf, xlsx, csv
+     */
     'drivers' => [
-        'pdf' => null,
-        'xlsx' => null,
-        'csv' => null,
+
+        'pdf' => [
+            'class' => null,
+            'config' => [
+                'orientation' => 'Landscape',
+                'format' => 'a4',
+            ]
+        ],
+
+
+        'xlsx' => [
+            'class' => null,
+            'config' => []
+        ],
+
+
+        'csv' => [
+            'class' => null,
+            'config' => []
+        ],
     ],
 
 ];
